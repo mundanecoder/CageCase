570cb889c317c61179ca45dad4efd469620c5e73
mundanecoder <dipankar.laskar45@gmail.com>
2024-05-19 22:18:53 +0530

    added the rnd library for resizing of the image

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AspectRatio } from "../../../components/ui/aspect-ratio";
+import NextImage from "next/image";
+import { Rnd } from "react-rnd";
+interface DesignConfiguratorProps {
+  configId: string;
+  imageUrl: string;
+  imageDimensions: {
+    width: number;
+    height: number;
+  };
+}
+const DesignConfigurator = ({
+  configId,
+  imageUrl,
+  imageDimensions,
+}: DesignConfiguratorProps) => {
+  return (
+    <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
+      <div className="relative h-[37.5] overflow-hidden col-span-2 w-full max-w-4xal flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+        <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+          <AspectRatio
+            ratio={896 / 1831}
+            className="pointer-events-none relative z-50 aspect-[896/1831] w-full"
+          >
+            <NextImage
+              src={"/phone-template.png"}
+              fill={true}
+              objectFit="contain"
+              alt="phone image"
+            />
+          </AspectRatio>
+          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
+          <div
+            className={cn(
+              "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
+              //   `bg-${options.color.tw}`
+              `bg-orange-950`
+            )}
+          />
+        </div>
+
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+        >
+          <div className="relative w-full h-full">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
+      </div>
+    </div>
+  );
+};
+
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,39 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 import React from "react";
+import DesignConfigurator from "./DesignConfigurator";
 
-const page = async () => {
-  return <div></div>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageUrl={imageUrl}
+      imageDimensions={{ height: height, width: width }}
+    />
+  );
 };
 
 export default page;
